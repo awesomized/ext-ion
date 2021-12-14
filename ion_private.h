f6fbc3a7f494cd0fe6156c49e8bf59e15c24bd3c
Michael Wallner <mike@php.net>
2021-12-14 12:39:47 +0100

    only check single char annotations

--- ion_private.h
+++ ion_private.h
@@ -1667,7 +1667,7 @@ static inline void php_ion_unserialize_annotations(php_ion_unserializer *ser)
 		ION_STRING ann_str;
 		ION_CHECK(ion_reader_get_an_annotation(ser->reader, i, &ann_str));
 
-		if (ann_str.length > 1) {
+		if (ann_str.length != 1) {
 			continue;
 		}
 
